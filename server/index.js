9a550e421fa903824082e3c31342832deb80d705
nchangizi <48898861+nchangizi@users.noreply.github.com>
2022-11-18 11:43:54 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 console.log("Script started");
 
 const pool = new Pool({
-  host: 'postgresql://ec2-18-215-41-121.compute-1.amazonaws.com/d69760tlssr9rr?ssl=false',
+  host: 'ec2-18-215-41-121.compute-1.amazonaws.com',
   user: 'raplhvqtreahld',
   port: '5432',
   password: 'a1ff5f231dd2747ac645573e92b36c6861b2cec959cc12d8f22b672b7f976742',
