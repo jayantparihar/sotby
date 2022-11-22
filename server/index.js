0183b8e19fd4d7765152706d87e73feb981e4894
Jayant Parihar <78824700+jayantparihar@users.noreply.github.com>
2022-11-22 12:10:00 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader("Content-Security-Policy-Report-Only: default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'");
+  res.setHeader('Content-Security-Policy': "default-src 'self'; script-src 'self' 'report-sample'; style-src 'self' 'report-sample'; base-uri 'none'; object-src 'none'; report-uri https://5e52f4c893efcda6a7d40460.endpoint.csper.io");
   next();
 });
 
