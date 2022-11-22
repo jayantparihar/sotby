90c66c2bfd8ced7611542ca55a197df0724f023c
Jayant Parihar <78824700+jayantparihar@users.noreply.github.com>
2022-11-22 11:42:38 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader("Content-Security-Policy", "default-src 'self' style-src 'unsafe-inline'");
+  res.setHeader("Content-Security-Policy", "default-src 'self';script-src 'self';object-src 'none';img-src 'self';media-src 'self';frame-src 'none';font-src 'self' data:;connect-src 'self';style-src 'self'");
   next();
 });
 
